9095662ebd457b2d8d05896418bc1a792106948a
Hailey Wong <73897167+hileelwb@users.noreply.github.com>
2022-01-06 12:21:26 -0500

    Create ToiletPaper.java

---
+++ Unit 6/6.1/ToiletPaper.java
@@ -0,0 +1,13 @@
+public class ToiletPaper{
+  private int squares;
+  private double price;
+
+  public ToiletPaper(int squares){
+    this.squares = squares;
+  }
+
+  public ToiletPaper(int squares, double price){
+    this.squares = squares;
+    this.price = price;
+  }
+}
