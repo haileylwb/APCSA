69d88888e86e2e4102c164cbedc25021997ce923
Hailey Wong <73897167+hileelwb@users.noreply.github.com>
2022-03-09 12:04:44 -0500

    Create Main.java

---
+++ Unit 9/9.4/Main.java
@@ -0,0 +1,7 @@
+class Main {
+  public static void main(String[] args) {
+    Fish f = new Fish(3); //Call a superclass constructor with super.
+    f.noise("yeehaw"); //Call a superclass method with super.
+    
+  }
+}
